ab49697fbc08d2afb247e38ac799ea4c06044625
Abdul Nasir <nuz.nasar@gmail.com>
2025-08-01 21:51:58 +0530

    Initial commit for landingpageC8R

--- config/index.ts
+++ config/index.ts
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
-  title: "John Doe | Space Portfolio",
+  title: "Cre8tar-Landing-page",
   description: "Welcome to my full stack Next.js 14 space portfolio.",
   keywords: [
     "reactjs",
@@ -35,7 +35,7 @@ export const siteConfig: Metadata = {
     "css",
   ] as Array<string>,
   authors: {
-    name: "Sanidhya Kumar Verma",
-    url: "https://github.com/sanidhyy",
+    name: "Nasir",
+    url: "https://cre8tar.com/",
   },
 } as const;
