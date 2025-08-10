2de240980d65d1ba68ff2ab1b803f2d6b4710f64
Santhosh Patel <85160404+santhosh-patel@users.noreply.github.com>
2025-08-10 11:20:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['your-domain.com'],
+    domains: ['cre8tar-test.vercel.app'],
   },
   reactStrictMode: true,
 };
